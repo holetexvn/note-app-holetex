25ecd5916e13ebf56a580383e0871896e0215a14
holetex <holetex@outlook.com>
2023-01-03 14:57:21 +0700

    clean

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,20 +1,5 @@
 import { GRAPHQL_SERVER } from './constants';
-import { createClient } from 'graphql-ws';
 
-const client = createClient({
-  url: 'ws://localhost:4000/graphql',
-});
-
-export async function execute(payload) {
-  return new Promise((resolve, reject) => {
-    let result;
-    client.subscribe(payload, {
-      next: (data) => (result = data),
-      error: reject,
-      complete: () => resolve(result),
-    });
-  });
-}
 export const graphQLRequest = async (payload, options = {}) => {
   if (localStorage.getItem('accessToken')) {
     const res = await fetch(`${GRAPHQL_SERVER}/graphql`, {
@@ -32,14 +17,3 @@ export const graphQLRequest = async (payload, options = {}) => {
     return data;
   }
 };
-
-// export const graphqlSubscriptionRequest = new SubscriptionClient(
-//   GRAPHQL_SUBSCRIPTION_ENDPOINT,
-//   {
-//     reconnect: true,
-//     lazy: true, // only connect when there is a query
-//     connectionCallback: (error) => {
-//       error && console.error(error);
-//     },
-//   }
-// );
