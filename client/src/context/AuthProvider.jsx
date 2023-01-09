9657bc5ab205026450f8fac23ea01bf62defd885
Tung Nguyen <tung.nguyen@doctoranywhere.com>
2023-01-09 10:16:56 +0700

    fix bug

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -1,26 +1,33 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState({});
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
   const auth = getAuth();
 
   useEffect(() => {
-    const unsubcribed = auth.onAuthStateChanged((user) => {
+    const unsubcribed = auth.onIdTokenChanged((user) => {
       console.log('[From AuthProvider]', { user });
       if (user?.uid) {
         setUser(user);
-        localStorage.setItem('accessToken', user.accessToken);
+        if (user.accessToken !== localStorage.getItem('accessToken')) {
+          localStorage.setItem('accessToken', user.accessToken);
+          window.location.reload();
+        }
+        setIsLoading(false);
         return;
       }
 
-      // reset user info 
-      console.log('reset')
+      // reset user info
+      console.log('reset');
+      setIsLoading(false);
       setUser({});
       localStorage.clear();
       navigate('/login');
@@ -29,12 +36,12 @@ export default function AuthProvider({ children }) {
     return () => {
       unsubcribed();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth]);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      {children}
+      {isLoading ? <CircularProgress /> : children}
     </AuthContext.Provider>
   );
 }
